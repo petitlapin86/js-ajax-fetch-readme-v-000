96955e97041076e0947c4af91d1f65e1de8c5689
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-27 17:24:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much.";
+
+
+
+
+const token = '39380f11db515d01709a391b20d05865f160cf64'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
